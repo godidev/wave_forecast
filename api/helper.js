ef97678f69f3d4324282a25070b7cbed785dc844
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-21 20:34:36 +0100

    feat: convert to monorepo

---
+++ api/helper.js
@@ -0,0 +1,35 @@
+const { writeFile } = require('fs')
+
+async function evaluateSelectors(selectors, page) {
+	return await page.evaluate(selectors => {
+		let resultados = {}
+		selectors.forEach(([type, selector, start, end]) => {
+			console.log({ type })
+			resultados = { ...resultados, name: type, [type]: [] }
+			const row = [...document.querySelectorAll(selector)].slice(
+				start,
+				end
+			)
+			row.forEach(item => {
+				console.log({ resultados })
+				resultados[type].push(item.textContent)
+			})
+		})
+		return resultados
+	}, selectors)
+}
+
+function saveToDb(file, data) {
+	writeFile(file, JSON.stringify(data, null, 2), err => {
+		err ? console.log(err) : console.log('File written successfully\n')
+	})
+}
+
+async function loadPageAndWait(browser, url) {
+	const page = await browser.newPage()
+	await page.goto(url)
+	await page.waitForLoadState('networkidle')
+	return page
+}
+
+module.exports = { saveToDb, loadPageAndWait, evaluateSelectors }
---
+++ api/index.js
@@ -0,0 +1,31 @@
+const { chromium } = require('playwright')
+const { saveToDb, loadPageAndWait, evaluateSelectors } = require('./helper')
+const { webPages } = require('./data')
+
+async function getDataFrom(browser, webPages) {
+	console.log({ webPages })
+	const allData = {}
+	for (const web in webPages) {
+		const { url, selectors } = webPages[web]
+		const page = await loadPageAndWait(browser, url)
+		allData[web] = await evaluateSelectors(selectors, page)
+
+		await page.close()
+	}
+
+	return allData
+}
+
+;(async () => {
+	const browser = await chromium.launch()
+	const forecast = { forecast: [] }
+	for (const spot in webPages) {
+		forecast.forecast.push({
+			name: spot,
+			webpages: await getDataFrom(browser, webPages[spot]),
+		})
+	}
+
+	saveToDb('./db/forecast.json', forecast)
+	await browser.close()
+})()
