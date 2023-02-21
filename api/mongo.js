1d470995965356983e16f8e65d883a32ff831c27
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-21 21:51:18 +0100

    feat: install server dependencies

---
+++ api/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const url = process.env.mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+	content: 'HTML is Easy',
+	important: true,
+})
+
+note.save().then(result => {
+	console.log('note saved!')
+	mongoose.connection.close()
+})
