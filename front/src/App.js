850b4c58f95f7a680a50ead9193d5966a65ba357
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-25 13:50:14 +0100

    feat: add styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Row from './components/Row'
-import DaysRow from './components/DaysRow'
+import Table from './components/Table/Table'
+import './App.css'
 
 function App() {
 	const [forecast, setForecast] = useState([])
@@ -14,40 +14,7 @@ function App() {
 
 	return (
 		<div className='App'>
-			{forecast.map(({ name, webpages }, index) => {
-				const { windguru, surfForecast } = webpages
-
-				return (
-					<div key={index}>
-						<h3>{name}</h3>
-						<table border={1}>
-							<thead>
-								<tr>
-									<th colSpan={7}>{name}</th>
-								</tr>
-								<DaysRow hours={surfForecast.time} />
-								<Row data={surfForecast.time} text='Hora' />
-							</thead>
-							<tbody>
-								<Row
-									data={windguru.waveHeight}
-									text='Altura'
-									magnitude='m'
-								/>
-								<Row
-									data={windguru.period}
-									text='Periodo'
-									magnitude='s'
-								/>
-								<Row
-									data={surfForecast.energy}
-									text='Energia'
-								/>
-							</tbody>
-						</table>
-					</div>
-				)
-			})}
+			<Table forecast={forecast} />
 		</div>
 	)
 }
---
+++ front/src/components/Table/Table.js
@@ -0,0 +1,27 @@
+import Row from './Row'
+import DaysRow from './DaysRow'
+import './Table.css'
+
+export default function Table({ forecast }) {
+	return forecast.map(({ name, webpages }, index) => {
+		const { windguru, surfForecast } = webpages
+
+		return (
+			<table key={index} className='table-content'>
+				<thead className='table-header'>
+					<DaysRow hours={surfForecast.time} />
+					<Row data={surfForecast.time} text='Hora' />
+				</thead>
+				<tbody className='table-body'>
+					<Row
+						data={windguru.waveHeight}
+						text='Altura'
+						magnitude='m'
+					/>
+					<Row data={windguru.period} text='Periodo' magnitude='s' />
+					<Row data={surfForecast.energy} text='Energia' />
+				</tbody>
+			</table>
+		)
+	})
+}
