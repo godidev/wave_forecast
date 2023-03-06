fa7377e2ee7461042873bc5e35e8fc647d53d1dc
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-03-06 19:09:29 +0100

    last fix

--- front/src/components/Table/Table.js
+++ front/src/components/Table/Table.js
@@ -6,10 +6,35 @@ export default function Table({ forecast }) {
 	console.log(forecast)
 	console.log('tipo:', typeof forecast)
 
+	let renderData = []
 	for (const beach in forecast) {
-		console.log({ beach })
+		renderData = beach.map(({ webpages }, index) => {
+			const { windguru, surfForecast } = webpages
+
+			return (
+				<table key={index} className='table-content'>
+					<thead className='table-header'>
+						<DaysRow hours={surfForecast.time} />
+						<Row data={surfForecast.time} text='Hora' />
+					</thead>
+					<tbody className='table-body'>
+						<Row
+							data={windguru.waveHeight}
+							text='Altura'
+							magnitude='m'
+						/>
+						<Row
+							data={windguru.period}
+							text='Periodo'
+							magnitude='s'
+						/>
+						<Row data={surfForecast.energy} text='Energia' />
+					</tbody>
+				</table>
+			)
+		})
 	}
-	return forecast.map(({ webpages }, index) => {
+	return renderData.map(({ webpages }, index) => {
 		const { windguru, surfForecast } = webpages
 
 		return (
